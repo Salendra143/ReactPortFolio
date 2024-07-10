937ac8db8c89fc3b8cbf13d8eb3413e80b77e7dd
Salendra143 <Salendra2114@gmail.com>
2024-07-10 19:40:35 +0530

    animate

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,11 +8,33 @@ import razorpay from "../projectimg/razorpay.png";
 import courses from "../projectimg/courses.png";
 import discord from "../projectimg/discord.png";
 import tours from "../projectimg/tours.png";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Project = () => {
+
+  useGSAP(()=>{
+    gsap.from(".images > *",{
+      opacity: 0,
+      z: -100,
+      duration: 1,
+      ease: 'power1.out',
+      stagger: 0.1,
+      scrollTrigger: {
+            trigger: '.main',
+            scroller: 'body',
+            start: 'top 50%',
+            end: 'bottom 10%',
+            scrub: true,
+          }
+    })
+
+
+  },[])
+
   return (
-    <div id="project" className=" w-full bg-black  py-5 italic text-white">
-      <div className="relative">
+    <div id="project" className="main w-full bg-black  py-5 italic text-white">
+      <div  className="images relative">
         <p className=" text-[10vw] cursor-pointer hover:text-cyan-400 sm:text-[4rem] lg:text-[5rem] italic text-center font-bold relative z-10">
           Personal Projects
         </p>
@@ -23,15 +45,15 @@ const Project = () => {
         </div>
       </div>
 
-      <div className=" flex justify-evenly gap-5 px-5 sm:px-20 mx-auto  flex-wrap ">
+      <div className="images flex justify-evenly gap-5 px-5 sm:px-20 mx-auto  flex-wrap ">
 
-        <div className="w-[200px] h-[200px] cursor-pointer relative md:w-[300px] md:h-[300px] rounded-2xl">
+        <div className="w-[200px] h-[200px] translate-z-6 transform skew-y-6  cursor-pointer relative md:w-[300px] md:h-[300px] rounded-2xl">
           <img
-            className="w-full h-full object-cover rounded-2xl"
+            className="w-full h-full translate-z-6 transform skew-y-6 object-cover rounded-2xl"
             src={youtube}
             alt=""
           />
-          <div className="absolute inset-0 bg-cyan-400 opacity-0 hover:opacity-100 transition-opacity duration-1000 font-bold text-black ease-in-out rounded-2xl flex sm:justify-center sm:items-center sm:text-center flex-col p-2  md:p-8 ">
+          <div className="absolute hover:translate-z-6 hover:transform hover:skew-y-6 inset-0 bg-cyan-400 opacity-0 hover:opacity-100 transition-opacity duration-1000 font-bold text-black ease-in-out rounded-2xl flex sm:justify-center sm:items-center sm:text-center flex-col p-2  md:p-8 ">
             <p className="text-xs md:text-lg">
               Here is my Video Streaming clone App using React. My app includes functionalities like auto suggestion search like youTube and live chat
               and many more.
